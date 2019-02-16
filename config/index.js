638879b530d7861681b54682958eb8f7520dc134
vistriter <vistriter@gmail.com>
2019-02-17 01:22:46 +0700

    add require package for develop electron app

--- config/index.js
+++ config/index.js
@@ -1,5 +1,6 @@
 const isDev = require('electron-is-dev')
 
 module.exports = {
-  frontendUrl: isDev ? 'http://localhost:3000' : 'https://app.coountdown.com',
+  APP_NAME: 'Coountdown',
+  FRONTEND_URL: isDev ? 'http://localhost:3000' : 'https://app.coountdown.com',
 }
