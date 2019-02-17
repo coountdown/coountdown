2169377c408b250125cc803439960d02e5f9ac2d
vistriter <vistriter@gmail.com>
2019-02-17 22:20:43 +0700

    add icon and add build script

--- config/index.js
+++ config/index.js
@@ -5,5 +5,5 @@ module.exports = {
   WEB_URL: isDev ? 'http://localhost:3001' : 'https://coountdown.com',
   APP_URL: isDev ? 'http://localhost:3000' : 'https://app.coountdown.com',
   SENTRY_DNS: 'https://16a7cc438d5e4184b518932b121748ee@sentry.io/1395662',
-  MIXPANEL_TOKEN: 'f9db20426301050e7ba821677b347166',
+  MIXPANEL_TOKEN: 'c327aa317e8b7c25aa30dd887fcf3ced'
 }
