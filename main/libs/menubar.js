2945aa3bdd1afd7e916d0c286723addbcf2d4427
vistriter <vistriter@gmail.com>
2019-02-17 12:44:17 +0700

    add menubar

---
+++ main/libs/menubar.js
@@ -0,0 +1,37 @@
+const menubar = require('menubar')
+const isDev = require('electron-is-dev')
+const mixpanel = require('./mixpanel')
+const config = require('../../config')
+
+module.exports = (tray, app) => {
+  const mb = menubar({
+    tray,
+    index: config.FRONTEND_URL,
+    height: 600,
+    width: 400,
+    movable: false,
+    resizable: false,
+    preloadWindow: true,
+    hasShadow: true,
+    transparent: true,
+    center: false,
+    darkTheme: true,
+    show: false,
+    webPreferences: {
+      devTools: isDev,
+    },
+  })
+
+  mb.showWindow()
+  mb.on('show', () => {
+    mixpanel.track(app, 'Active App')
+  })
+
+  mb.on('after-create-window', () => {
+    // Can open developer tools
+  })
+
+  mb.on('after-show', () => {})
+
+  return mb
+}
