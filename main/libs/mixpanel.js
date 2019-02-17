1a5fdb9e1332701bc6852b7aeadd6227559c3168
vistriter <vistriter@gmail.com>
2019-02-17 19:41:09 +0700

    add user id and update total session

--- main/libs/mixpanel.js
+++ main/libs/mixpanel.js
@@ -12,18 +12,15 @@ module.exports = {
       const currentMachineId = await machineId()
       const appVersion = app && 'getVersion' in app ? app.getVersion() : 'Unknown'
 
-      mixpanel.people.set(currentMachineId, Object.assign(
-        {
-          distinct_id: currentMachineId,
-          name: 'Anonymous',
-          app_version: appVersion,
-          electron_version: process.versions.electron,
-          process: process.type,
-          platform: os.platform(),
-          platform_release: os.release(),
-        },
-        props,
-      ))
+      mixpanel.people.set(currentMachineId, {
+        distinct_id: currentMachineId,
+        name: 'Anonymous',
+        app_version: appVersion,
+        electron_version: process.versions.electron,
+        process: process.type,
+        platform: os.platform(),
+        platform_release: os.release(),
+      })
 
       // Dont' overwrite old properties
       mixpanel.people.set_once(currentMachineId, { created_at: moment().format('LLLL') })
@@ -46,5 +43,13 @@ module.exports = {
     } catch (err) {
       console.error(err)
     }
+  },
+  addUserId: async (userId) => {
+    const currentMachineId = await machineId()
+    mixpanel.people.set(currentMachineId, { user_id: userId })
+  },
+  increment: async (session) => {
+    const currentMachineId = await machineId()
+    mixpanel.people.increment(currentMachineId, 'total_session', session)
   },
 }
