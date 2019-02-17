9f5ed1932530d6365cbac5c978d83f31100d958e
vistriter <vistriter@gmail.com>
2019-02-17 11:09:32 +0700

    setup mixpanel

--- config/index.js
+++ config/index.js
@@ -4,4 +4,5 @@ module.exports = {
   APP_NAME: 'Coountdown',
   FRONTEND_URL: isDev ? 'http://localhost:3000' : 'https://app.coountdown.com',
   SENTRY_DNS: 'https://16a7cc438d5e4184b518932b121748ee@sentry.io/1395662',
+  MIXPANEL_TOKEN: 'f9db20426301050e7ba821677b347166',
 }
---
+++ main/libs/mixpanel.js
@@ -0,0 +1,50 @@
+const os = require('os')
+const Mixpanel = require('mixpanel')
+const { machineId } = require('node-machine-id')
+const moment = require('moment')
+const config = require('../../config')
+
+const mixpanel = Mixpanel.init(config.MIXPANEL_TOKEN)
+
+module.exports = {
+  track: async (app, event, props, callback) => {
+    try {
+      const currentMachineId = await machineId()
+      const appVersion = app && 'getVersion' in app ? app.getVersion() : 'Unknown'
+
+      mixpanel.people.set(currentMachineId, Object.assign(
+        {
+          distinct_id: currentMachineId,
+          name: 'Anonymous',
+          app_version: appVersion,
+          electron_version: process.versions.electron,
+          process: process.type,
+          platform: os.platform(),
+          platform_release: os.release(),
+        },
+        props,
+      ))
+
+      // Dont' overwrite old properties
+      mixpanel.people.set_once(currentMachineId, { created_at: moment().format('LLLL') })
+
+      mixpanel.track(
+        event,
+        Object.assign(
+          {
+            distinct_id: currentMachineId,
+            app_version: appVersion,
+            electron_version: process.versions.electron,
+            process: process.type,
+            platform: os.platform(),
+            platform_release: os.release(),
+          },
+          props,
+        ),
+        callback,
+      )
+    } catch (err) {
+      console.error(err)
+    }
+  },
+}
