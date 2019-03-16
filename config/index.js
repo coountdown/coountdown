cb293d29b9fea49818b0268de320e7dcc1191b6e
Ta Wei <vistriter@gmail.com>
2019-03-16 14:01:34 +0700

    update new version

--- config/index.js
+++ config/index.js
@@ -5,5 +5,5 @@ module.exports = {
   WEB_URL: isDev ? 'http://localhost:3001' : process.env.REACT_APP_WEB_URL,
   APP_URL: isDev ? 'http://localhost:3000' : process.env.REACT_APP_APP_URL,
   SENTRY_DNS: isDev ? 'https://16a7cc438d5e4184b518932b121748ee@sentry.io/1395662' : process.env.REACT_APP_SENTRY_DNS,
-  MIXPANEL_TOKEN: isDev ? 'f9db20426301050e7ba821677b347166' : process.env.REACT_APP_MIXPANEL_TOKEN,
+  MIXPANEL_TOKEN: isDev ? 'f9db20426301050e7ba821677b347166' : process.env.REACT_APP_MIXPANEL,
 }
