b409d9a74577f464c47d5db93f227bcc6009692d
Ta Wei <vistriter@gmail.com>
2019-03-16 15:08:48 +0700

    fix env

--- config/index.js
+++ config/index.js
@@ -1,9 +1,16 @@
 const isDev = require('electron-is-dev')
 
 module.exports = {
-  APP_NAME: 'Coountdown',
-  WEB_URL: isDev ? 'http://localhost:3001' : process.env.REACT_APP_WEB_URL,
-  APP_URL: isDev ? 'http://localhost:3000' : process.env.REACT_APP_APP_URL,
-  SENTRY_DNS: isDev ? 'https://16a7cc438d5e4184b518932b121748ee@sentry.io/1395662' : process.env.REACT_APP_SENTRY_DNS,
-  MIXPANEL_TOKEN: process.env.REACT_APP_MIXPANEL || 'f9db20426301050e7ba821677b347166',
+  // eslint-disable-next-line arrow-body-style
+  getEnv: (env) => {
+    return {
+      APP_NAME: 'Coountdown',
+      WEB_URL: isDev ? 'http://localhost:3001' : env.REACT_APP_WEB_URL,
+      APP_URL: isDev ? 'http://localhost:3000' : env.REACT_APP_APP_URL,
+      SENTRY_DNS: isDev
+        ? 'https://16a7cc438d5e4184b518932b121748ee@sentry.io/1395662'
+        : env.REACT_APP_SENTRY_DNS,
+      MIXPANEL_TOKEN: isDev ? 'f9db20426301050e7ba821677b347166' : env.REACT_APP_MIXPANEL,
+    }
+  },
 }
